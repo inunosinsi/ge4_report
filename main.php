ccc8573fd352893c186affa05b0d3dbd3a8f8537
inunosinsi <tsuyoshi@saitodev.co>
2025-03-20 05:36:12 +0900

    It has been confirmed that runReport works.

--- main.php
+++ main.php
@@ -1,4 +1,7 @@
 <?php
+/****************************************************************************************/
+/* https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=ja */
+/****************************************************************************************/
 
 require __DIR__."/vendor/autoload.php";
 use Google\Analytics\Data\V1beta\Client\BetaAnalyticsDataClient;
@@ -12,22 +15,42 @@ include_once(__DIR__."/config.php");
 putenv("GOOGLE_APPLICATION_CREDENTIALS=".__DIR__."/json/".CREDENTIALS.".json");
 $client = new BetaAnalyticsDataClient();
 
-$request = new RunReportRequest([
-    'property' => 'properties/'.PROPERTY_ID,
-    'dateRanges' => [
-   		new DateRange([
-   			'start_date' => '30daysAgo',
-   			'end_date' => 'today',
-   		]),
-   	],
-   
-   	'dimensions' => [
-   		new Dimension(['name' => "defaultChannelGroup"])
-   	],
-   	'metrics' => [
-   		new Metric(['name' => "keyEvents"])
-   	]
-]);
+$request = (new RunReportRequest())
+        ->setProperty('properties/' . PROPERTY_ID)
+        ->setDateRanges([
+            new DateRange([
+                'start_date' => '2024-09-01',
+                'end_date' => '2024-09-15',
+            ]),
+        ])
+        ->setDimensions([
+            new Dimension([
+                'name' => 'defaultChannelGroup',
+            ]),
+            new Dimension([
+            	'name' => 'sourceMedium',
+            ]),
+            new Dimension([
+            	'name' => 'eventName'
+            ]),
+            new Dimension([
+            	'name' => 'landingPage'
+            ]),
+            new Dimension([
+            	'name' => 'unifiedPagePathScreen'	
+            ])
+        ])
+        ->setMetrics([
+            new Metric([
+                'name' => 'keyEvents',
+            ]),
+            new Metric([
+            	'name' => 'sessions',
+            ]),
+            new Metric([
+            	'name' => 'totalUsers'
+            ])
+        ]);
 
 $res = $client->runReport($request);
 var_dump($res);
